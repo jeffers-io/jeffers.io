0f209b315e3f3bd11039b0dfdbf583c3dc8932cc
James Healy <jamesjeffershealy@gmail.com>
2019-06-06 17:30:32 -0400

    Cloudfront distro and A Record Working
    + Fixed build script to copy all assets

---
+++ build.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+set -euox pipefail
+
+rm -rf dist && \
+mkdir -p dist/assets/ && \
+cp -v index.html dist/ && \
+npx postcss --verbose index.css > dist/index.css && \
+npx svgo -f assets/ -o dist/assets/ && \
+find assets -not -name "*.svg" -type f -exec cp -v {} dist/assets \;
